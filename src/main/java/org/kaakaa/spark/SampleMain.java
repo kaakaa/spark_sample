1d550cf7e13752485d7f716a649a1c38e8c42836
kaakaa <stooner.hoe@gmail.com>
2015-03-01 00:17:30 +0900

    ok

--- src/main/java/org/kaakaa/spark/SampleMain.java
+++ src/main/java/org/kaakaa/spark/SampleMain.java
@@ -1,9 +1,11 @@
 package org.kaakaa.spark;
 
 import static spark.Spark.*;
+import spark.servlet.SparkApplication;
 
-class SampleMain {
-	public static void main(String[] args) {
+public class SampleMain implements SparkApplication{
+	@Override
+	public void init(){
 		get("/hello", (req, res) -> "hello");
 	}
 }
